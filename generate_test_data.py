67a90710d937965858d936ef0de208c114978a58
Apfelholz <149402866+Apfelholz@users.noreply.github.com>
2024-10-29 15:04:08 +0100

    feat: Add test data generator

---
+++ generate_test_data.py
@@ -0,0 +1,27 @@
+import random
+import argparse
+
+# Command Line Argument Parser
+parser = argparse.ArgumentParser(
+    prog="generate_test_data",
+    description="Generates n lines of sleep() statements within the specified rang",
+)
+
+parser.add_argument(
+    "-n", default=100, type=int, help="Number of lines to generate"
+)
+parser.add_argument(
+    "-t",
+    default="0.5-5",
+    help="Range for sleep times, formatted as lower-upper",
+)
+
+# Initialisieren der Grenzen und Argumente
+args = parser.parse_args()
+
+lower_bound, upper_bound = map(float, args.t.split("-"))
+lines = args.n
+
+# Test erstellen
+for i in range(lines):
+    print("sleep " + str(round(random.uniform(lower_bound, upper_bound), 2)))
