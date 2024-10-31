14e5702bbd0e2ff082edc1ee025a59e06b839335
Apfelholz <149402866+Apfelholz@users.noreply.github.com>
2024-10-31 14:51:42 +0100

    feat(cat): read aut of stdin

--- cat.py
+++ cat.py
@@ -1,17 +1,26 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(
     prog="cat",
-    description="reads and prints a log file line by line",
+    description="Reads and prints a log file line by line, or from stdin if no file is provided."
 )
 
-parser.add_argument("logfile")
+parser.add_argument("logfile", nargs="?", help="Path to the log file")
 args = parser.parse_args()
 
-logfile = args.logfile
-
-with open(logfile, "r", encoding="utf-16") as file:
-    lines = file.readlines()
+if args.logfile:
+    # read aut of file
+    with open(args.logfile, "r", encoding="utf-16") as file:
+        lines = file.readlines()
+    
     for line in lines:
-        cleaned_line = line.replace("\x00", "")  # Entfernt alle Nullbytes
+        cleaned_line = line.replace("\x00", "") 
         print(cleaned_line, end="")
+    
+else:
+    # read aut of stdin
+    lines = sys.stdin.read().splitlines()
+    
+    for line in lines:
+        print(line)
